936feb3d9db5d4837661128a1ee7782b4580df97
Ali Atallah Faris Al Sariera <sariera@live.com>
2018-12-08 20:46:19 +0200

    Create database and create a shortcut for easier to update database

--- lotsofitems.py
+++ lotsofitems.py
@@ -1,3 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Categories, CategoryItem, User
+
+# Create database and create a shortcut for easier to update database
+engine = create_engine('sqlite:///catalogs.db')
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
