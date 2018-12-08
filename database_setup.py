4a0f9be332fd6d5045963e19678b2ddec0314cf6
Ali Atallah Faris Al Sariera <sariera@live.com>
2018-12-08 20:41:50 +0200

    creating class user

--- database_setup.py
+++ database_setup.py
@@ -4,3 +4,12 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
