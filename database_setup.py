868da197b9a400e230b9206309a1b0fbab4974d7
Ali Atallah Faris Al Sariera <sariera@live.com>
2018-12-08 20:42:35 +0200

    creating class Categories

--- database_setup.py
+++ database_setup.py
@@ -13,3 +13,19 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
     email = Column(String(250), nullable=False)
+
+
+class Categories(Base):
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
